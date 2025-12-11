3a1075a02ac52e93db7aa4cec79d0a1727b8352c
Sulekha <sulekha@myhopehousing.org.uk>
2025-12-11 17:20:54 +0000

    ok

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,18 @@
 import '../styles/globals.css';
+import { useRouter } from 'next/router';
 import AppLayout from '../components/AppLayout';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  
+  // Pages that should NOT have the AppLayout authentication check
+  const publicPages = ['/login', '/set-password'];
+  const isPublicPage = publicPages.includes(router.pathname);
+  
+  if (isPublicPage) {
+    return <Component {...pageProps} />;
+  }
+  
   return (
     <AppLayout>
       <Component {...pageProps} />
