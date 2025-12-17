0ecede5664581dbc84b6dde93290b2a481345254
Sulekha <sulekha@myhopehousing.org.uk>
2025-12-17 11:46:47 +0000

    olaf

--- components/AppLayout.jsx
+++ components/AppLayout.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { supabase } from './supabaseClient';
+import { supabase } from '@/lib/supabaseClient';
 import {
   Home,
   Users,
