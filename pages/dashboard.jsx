649bb943788904d22dd1cb014612da7243fe83f7
Sulekha <sulekha@myhopehousing.org.uk>
2025-12-21 15:17:35 +0000

    wap

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect, useCallback } from "react";
 import { supabase } from "@/lib/supabaseClient";
+import ProtectedRoute from "../components/ProtectedRoute";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
