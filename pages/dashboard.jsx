c0003ae7ff4bae395b0b18940afa06f3358b16bf
Sulekha <sulekha@myhopehousing.org.uk>
2025-12-21 16:01:59 +0000

    was

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect, useCallback } from "react";
 import { supabase } from "@/lib/supabaseClient";
-import ProtectedRoute from "../components/ProtectedRoute";
+import ProtectedPage from "../components/ProtectedPage";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
