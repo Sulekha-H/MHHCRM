febf0de9b858b72cc9e2abee0872fece44f0baa0
Sulekha <sulekha@myhopehousing.org.uk>
2025-12-23 15:10:58 +0000

    ftea

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,29 @@
-import { requireAuth } from '../lib/requireAuth';
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
 
-export const getServerSideProps = requireAuth;
+export const getServerSideProps = async () => {
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        getAll: () => cookies().getAll(),
+        setAll: () => {},
+      },
+    }
+  );
 
-export default function Home() {
-  return <div>Dashboard</div>;
-}
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    return {
+      redirect: { destination: '/login', permanent: false },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+};
