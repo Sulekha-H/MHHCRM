ee288e65c6eb57f81e085e0b99a79ca1a448d24d
Sulekha <sulekha@myhopehousing.org.uk>
2025-12-23 15:02:40 +0000

    finaaksspp

--- pages/accommodations.jsx
+++ pages/accommodations.jsx
@@ -2,7 +2,6 @@
 
 import React, { useState, useEffect, useCallback, useMemo } from "react";
 import { supabase } from "@/lib/supabaseClient";
-import { requireAuth } from '../lib/requireAuth';
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
