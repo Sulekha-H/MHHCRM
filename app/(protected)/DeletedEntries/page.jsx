336685a2bb630dec4771e8e3ee877f46f7f07731
Sulekha <sulekha@myhopehousing.org.uk>
2025-12-24 13:02:50 +0000

    comty

--- app/(protected)/DeletedEntries/page.jsx
+++ app/(protected)/DeletedEntries/page.jsx
@@ -1,3 +1,4 @@
+"use Client"
 import React, { useState, useEffect } from "react";
 import { supabase } from "../lib/supabaseClient";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
