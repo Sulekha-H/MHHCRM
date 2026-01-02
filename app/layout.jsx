640b71e4586454cfcaa860823ed8e65b41ce7667
Sulekha <sulekha@myhopehousing.org.uk>
2026-01-02 15:57:55 +0000

    gpickksssssssssleanmangun

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,12 @@
-import "./global.css"
+// app/layout.jsx
+import "./global.css";
+import AppLayout from "../components/AppLayout"; // Adjust path if AppLayout is elsewhere
 
 export default function Rootlayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen">
-        {children}
+      <body>
+        <AppLayout>{children}</AppLayout>
       </body>
     </html>
   );
