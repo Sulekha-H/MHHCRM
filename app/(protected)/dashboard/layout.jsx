20b217688c523dbf105cde97afcacd5cb26d3f05
Sulekha <sulekha@myhopehousing.org.uk>
2026-01-02 12:36:47 +0000

    gpickksssssssssssssssszzzzzzzsfydbvnvjs

--- app/(protected)/dashboard/layout.jsx
+++ app/(protected)/dashboard/layout.jsx
@@ -1,5 +1,27 @@
-import AppLayout from "../AppLayout";
+import { redirect } from "next/navigation";
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+import AppLayout from "@/components/AppLayout";
 
-export default function DashboardLayout({ children }) {
+export default async function Layout({ children }) {
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        getAll: () => cookies().getAll(),
+        setAll: () => {},
+      },
+    }
+  );
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    redirect("/login");
+  }
+
   return <AppLayout>{children}</AppLayout>;
 }
