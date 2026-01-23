33ee9c7d308b2168b2512cca7dbefd11e722a55b
Sulekha <sulekha@myhopehousing.org.uk>
2026-01-23 12:10:57 +0000

    skdjcwsbsbv

--- middleware.ts
+++ middleware.ts
@@ -11,8 +11,6 @@ export const proxy = clerkMiddleware(async (auth, req) => {
   }
 });
 
-export default proxy;
-
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
