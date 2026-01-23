2271558f5bfa90137931ea5a147224cde42e3ee8
Sulekha <sulekha@myhopehousing.org.uk>
2026-01-23 12:07:47 +0000

    snjnskjskduuhy

--- middleware.ts
+++ middleware.ts
@@ -16,6 +16,6 @@ export default proxy;
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
+    '/(api|trpc)(.*)'
   ],
 };
