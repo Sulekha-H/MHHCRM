c64940bfd19392fe7611acb41e029de4f29b4216
Sulekha-H <sulekha@myhopehousing.org.uk>
2026-01-27 11:25:36 +0000

    Update page.jsx

--- app/sign-in/page.jsx
+++ app/sign-in/page.jsx
@@ -1,16 +1,22 @@
 "use client";
-import { SignIn } from "@clerk/nextjs";
+
+import { SignIn, SignedIn, SignedOut } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export default function SignInPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-50">
-      <SignIn
-        routing="path"
-        path="/sign-in"
-        RedirectUrl="/dashboard"
-        signUpUrl="/sign-up"
-        
-      />
+      <SignedIn>
+        {redirect("/dashboard")}
+      </SignedIn>
+
+      <SignedOut>
+        <SignIn
+          routing="path"
+          path="/sign-in"
+          signUpUrl="/sign-up"
+        />
+      </SignedOut>
     </div>
   );
 }
