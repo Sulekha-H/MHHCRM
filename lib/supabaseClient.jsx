cf08c6049e3dfbf3852d5e615829d6107f9e04a0
Sulekha-H <sulekha@myhopehousing.org.uk>
2026-02-02 14:24:10 +0000

    Update supabaseClient.jsx

--- lib/supabaseClient.jsx
+++ lib/supabaseClient.jsx
@@ -1,5 +1,7 @@
 // lib/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
+import { useAuth } from '@clerk/nextjs';
+
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
@@ -7,3 +9,17 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 export const supabase = (supabaseUrl && supabaseAnonKey)
   ? createClient(supabaseUrl, supabaseAnonKey)
   : null;
+
+const getSupabaseClient = async () => {
+    const token = await getToken({ template: 'supabase' });
+    return createClient(supabaseUrl, supabaseAnonKey, {
+      global: {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    });
+  };
+
+  return { getSupabaseClient };
+};
