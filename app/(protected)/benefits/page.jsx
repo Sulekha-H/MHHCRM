39d89a4cde1f57ffb1e318a171a892357cdae61a
Sulekha-H <sulekha@myhopehousing.org.uk>
2026-02-05 11:36:04 +0000

    Replace supabase import with useClerkSupabaseClient

--- app/(protected)/benefits/page.jsx
+++ app/(protected)/benefits/page.jsx
@@ -3,7 +3,7 @@
 
 import { useUser } from "@clerk/nextjs";
 import React, { useState, useEffect, useCallback } from "react";
-import { supabase } from "@/lib/supabaseClient";
+import { useClerkSupabaseClient } from "@/lib/supabaseClient";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
