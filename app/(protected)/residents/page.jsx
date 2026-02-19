da75b96760a276ac971d908ba6df1cd16a39f33d
Sulekha-H <sulekha@myhopehousing.org.uk>
2026-02-19 11:28:48 +0000

    Update page.jsx

--- app/(protected)/residents/page.jsx
+++ app/(protected)/residents/page.jsx
@@ -3,7 +3,7 @@
 
 import React, { useState, useEffect } from "react";
 import { useSession, useUser } from '@clerk/nextjs'
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "lib/supabaseClient.jsx";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
