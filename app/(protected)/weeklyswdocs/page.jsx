482dc98c3e86c89df1f9701361c2ca158344d97c
Sulekha-H <sulekha@myhopehousing.org.uk>
2026-03-16 15:04:38 +0000

    Update page.jsx

--- app/(protected)/weeklyswdocs/page.jsx
+++ app/(protected)/weeklyswdocs/page.jsx
@@ -266,7 +266,7 @@ useEffect(() => {
         
         // Filter logs to only include the documents we're tracking
         const requiredDocsNames = [
-            "Weekly insepections",
+            "Weekly inpections",
             "Weekly schedule"
         ];
         const trackedDocumentIds = swDocuments
